0d9e14b6d4e7e58b22bc1b0c2aeb5ddacde1494b
hushcut <rechardmin@gmail.com>
2025-05-14 14:06:12 +0900

    users 앱 만들고 url 설정 틀만잡아둔거라 삭제해도 괜찮음

--- teams/urls.py
+++ teams/urls.py
@@ -1,5 +1,20 @@
+import views
 from django.urls import path
 from . import views
+
+app_name = 'users'
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('register/',
+         views.register_user, name='register'),  # 회원가입
+    path('login/', views.login_user,
+         name='login'),  # 로그인
+    path('logout/', views.logout_user,
+         name='logout'),  # 로그아웃
+]
 
 urlpatterns = [
     path('', views.team_list, name='team_list'),
--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+def register_user(request):
+    if request.method == 'POST':  # POST 요청일 경우 폼 처리
+        form = UserCreationForm(request.POST)
+        if form.is_valid():  # 폼이 유효하면
+            form.save()  # 유저를 저장
+            messages.success(request, "회원가입이 완료되었습니다!")
+            return redirect('users:login')  # 로그인 페이지로 리디렉션
+    else:  # GET 요청일 경우 빈 폼 보여주기
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
 
 # Create your views here.
